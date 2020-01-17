bfe196838b28f5f9c8527d986610071a8b117deb
konman1989 <manoilo.blog@gmail.com>
2020-01-17 17:29:54 +0200

    updated

--- main.py
+++ main.py
@@ -2,52 +2,58 @@ import re
 import requests
 from telebot import TeleBot, types
 
+bot = TeleBot('914056937:AAHiLz8n6PVBb10YCQh7eBo9JB9W2kAtWJE')
 
-bot = TeleBot('982217890:AAHU7pedco6HUQfIUy_nqR2EyPQ3pjVS2WE')
-markup = types.ReplyKeyboardMarkup(row_width=2)
 
 @bot.message_handler()
 def get_definition(message):
+    def text_splitter():
+        """Splits text into chunks < 4096 to follow telegram message
+        length rules"""
+
+        text_list = text.split('\n\n')
+        new_text = ""
+        for chunk in text_list:
+            if chunk:
+                if len(new_text + chunk) < 4096:
+                    new_text += "\n\n" + chunk
+                else:
+                    bot.send_message(message.chat.id, new_text)
+                    new_text = ""
+                    new_text += "\n\n" + chunk
+        bot.send_message(message.chat.id, new_text)
+
+    if message.text == '/start':
+        bot.send_message(message.chat.id,
+                         'Give me a definition you would like to get:')
+        return
     req = requests.get(
         f'http://api.urbandictionary.com/v0/define?term={message.text}")')
 
     text = ""
-
-    for _ in req.json().get('list'):
-        text += _.get('definition')
+    for index, _ in enumerate(req.json().get('list'), start=1):
+        text += f"\n\nDefinition {index}:\n{_.get('definition')}"
 
     result = re.finditer(r'\[\w+\]', text)
+    definitions = [r.group()[1:-1] for r in result]
 
-    bot.send_message(message.chat.id, text)
+    text = text.replace('[', '').replace(']', '')
 
-    definitions = []
+    if len(text) > 4096:
+        text_splitter()
+    else:
+        bot.send_message(message.chat.id, text)
 
-    for _ in result:
-        definitions.append(_.group()[1:-1])
+    markup = types.ReplyKeyboardMarkup()
 
-    # for item in range(0, 12, 3):
-    #     word1 = types.KeyboardButton(definitions[item])
-    #     word2 = types.KeyboardButton(definitions[item + 1])
-    #     word3 = types.KeyboardButton(definitions[item + 2])
-    #     markup.row(word1, word2, word3)
+    for n in range(0, len(definitions), 3):
+        buttons = (definitions[n:n + 3])
+        markup.row(*buttons)
 
-    for word in definitions[:10]:
-        word = types.KeyboardButton(word)
-        markup.add(word)
-
-    bot.send_message(message.chat.id, "Choose a word:",
-                     reply_markup=markup)
-
-    req = requests.get(
-        f'http://api.urbandictionary.com/v0/define?term={message.text}")')
-
-    text = ""
-
-    for _ in req.json().get('list'):
-        text += _.get('definition')
-
-    bot.send_message(message.chat.id, text)
+    bot.send_message(message.chat.id, '_Choose a word from the list below:_',
+                     reply_markup=markup,
+                     parse_mode='MarkdownV2')
 
 
 if __name__ == '__main__':
-    bot.polling()
+    bot.polling(none_stop=True)
